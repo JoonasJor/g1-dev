70f13c09f387eae1f4f66a01c086208d46c89c3b
JoonasJor <jormanainen.joonas@gmail.com3>
2025-09-16 14:40:38 +0300

    update logging and add data playback

---
+++ logger/playback.py
@@ -0,0 +1,78 @@
+import os 
+import sys
+import threading
+import numpy as np
+import time
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from unitree_sdk2py.core.channel import ChannelFactoryInitialize
+from unitree_sdk2py.utils.thread import RecurrentThread
+from unitree_sdk2py.idl.unitree_hg.msg.dds_ import LowState_
+
+from controller import BodyController, ArmController, HandController
+
+import inspire.inspire_dds as inspire_dds
+from inspire.modbus_data_handler import ModbusDataHandler
+
+from camera.camera_subscriber import CameraSubscriber
+from camera.utils import decode_image
+import camera.camera_dds as camera_dds
+
+from logger.g1_logger import G1_Logger
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        # Run on real robot
+        ChannelFactoryInitialize(0, sys.argv[1])
+        modbus_r = ModbusDataHandler(ip="192.168.123.211", device_id=1, l_r="r")
+        modbus_l = ModbusDataHandler(ip="192.168.123.210", device_id=2, l_r="l")
+    else:
+        # Run in Mujoco
+        ChannelFactoryInitialize(1, "lo")
+
+    body = BodyController(flg_initialized=True)
+    arms = ArmController()
+    hand_r = HandController(l_r="r")
+    hand_l = HandController(l_r="l")
+
+    data_list = G1_Logger.load_data_from_file("log/data_latest.pkl")
+    
+    for i, data in enumerate(data_list):
+        timestamp = data_list[i]["timestamp"]
+        timestamp_next = data_list[i + 1]["timestamp"] if i + 1 < len(data_list) else None
+
+        body_low_state: LowState_ = data["body"]
+        body_joint_angles = [motor.q for motor in body_low_state.motor_state]
+
+        hand_r_state: inspire_dds.inspire_hand_state = data["hand_r"]["state"]
+        hand_r_joint_angles = hand_r_state.angle_act
+
+        hand_l_state: inspire_dds.inspire_hand_state = data["hand_l"]["state"]
+        hand_l_joint_angles = hand_l_state.angle_act
+
+        camera_data: camera_dds.camera_image = data["camera"]
+
+        if i == 0:
+            print("Moving to initial position...")
+            hand_r.low_cmd_control(hand_r_joint_angles, duration=2.0)
+            hand_l.low_cmd_control(hand_l_joint_angles, duration=2.0)
+            body.low_cmd_control(body_joint_angles, duration=2.0)
+            body.repeat_last_command()
+            continue
+
+        if timestamp_next is not None:
+            timestamp_delta = timestamp_next - timestamp
+            print(timestamp_delta)
+
+            threads = [
+                threading.Thread(target=body.low_cmd_control, args=(body_joint_angles,), kwargs={"duration": timestamp_delta, "interpolate": False}),
+                threading.Thread(target=hand_r.low_cmd_control, args=(hand_r_joint_angles,), kwargs={"duration": timestamp_delta}),
+                threading.Thread(target=hand_l.low_cmd_control, args=(hand_l_joint_angles,), kwargs={"duration": timestamp_delta}),
+            ]
+
+            for t in threads:
+                t.start()
+            for t in threads:
+                t.join()
+                
